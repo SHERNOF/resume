96309547bcb8001abb28a03803f84b57f90e24e4
shernof <sherwin.nofuente@yahoo.com>
2020-04-20 00:49:29 +1000

    1

--- js/app.js
+++ js/app.js
@@ -6,3 +6,44 @@ $('#profile_ripple').ripples({
 })
 
 })
+
+
+// stats
+
+const counter  = document.querySelectorAll('.counter');
+const speed = 200;
+
+counter.forEach(counter => {
+    const updateCount = () => {
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+        console.log(target);
+        const inc = target / speed;
+        
+        
+
+        if(count < target) {
+            counter.innerText = Math.ceil(count + inc);
+            setTimeout(updateCount, 1)
+        } else {
+            count.innerText = target
+        }
+
+        // console.log(inc);
+        // console.log(count);
+    }
+    updateCount();
+
+    // $('.stats').hover(function(){
+    //   const a = 0
+    //   let b = document.querySelectorAll('.counter').innerText = a;
+    //   console.log(b)
+
+    //   if(b = '0'){
+    //     updateCount();
+    //   }
+
+    // })
+
+})
+
